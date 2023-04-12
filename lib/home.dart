76d842ef8409a5c3061ed53835ad4775e3b4540a
deepak230902 <deepakdennydavid@gmail.com>
2023-04-12 15:30:56 +0530

    homepg

--- lib/home.dart
+++ lib/home.dart
@@ -5,7 +5,6 @@ import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:page_transition/page_transition.dart';
 
 class Homescreen extends StatefulWidget {
-
   const Homescreen({super.key});
 
   @override
@@ -13,48 +12,87 @@ class Homescreen extends StatefulWidget {
 }
 
 class _HomescreenState extends State<Homescreen> {
-  int _selectedIndex=0;
+  int _selectedIndex = 0;
 
-  void _ontapped(int index)
-  {
-    setState(() 
-    {
-      _selectedIndex=index;
+  void _ontapped(int index) {
+    setState(() {
+      _selectedIndex = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Column(children: [    ElevatedButton(onPressed: ()
-      {
-        Navigator.pushAndRemoveUntil(context, PageTransition(type: PageTransitionType.leftToRightWithFade,
-        child: const LoginScreen()), (route) => false);
-      }, child: const Text('LOGOUT'))],)),
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      bottomNavigationBar: BottomNavigationBar(items: const <BottomNavigationBarItem>[
-      BottomNavigationBarItem(icon: Icon(Icons.home),label: 'home'),
-      BottomNavigationBarItem(icon: Icon(Icons.person),label: 'profile'),
-      BottomNavigationBarItem(icon: Icon(Icons.settings),label: 'settings')],
-      currentIndex: _selectedIndex,
-      selectedItemColor: Colors.indigo,
-      onTap: _ontapped,
-
-     ),
-      
+      backgroundColor: Colors.black,
+      body: SafeArea(
+          child: Column(
+        children: [
+          ElevatedButton(
+              onPressed: () {
+                Navigator.pushAndRemoveUntil(
+                    context,
+                    PageTransition(
+                        type: PageTransitionType.leftToRightWithFade,
+                        child: const LoginScreen()),
+                    (route) => false);
+              },
+              child: const Text('LOGOUT')),
+          const SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Container(
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.white),
+                    borderRadius: const BorderRadius.all(Radius.circular(5))),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        const Text(
+                          'ADD YOUR ',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        const Text(
+                          'VEHICLE',
+                          style: TextStyle(color: Colors.red),
+                        ),
+                        SizedBox(width: 170),
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: const Icon(
+                            Icons.add,
+                            color: Colors.white,
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            shape: const CircleBorder(),
+                            padding: const EdgeInsets.all(20),
+                            backgroundColor: Colors.red,
+                          ),
+                        )
+                      ],
+                    ),
+                    Image.asset('assets/car.jpg'),
+                  ],
+                )),
+          )
+        ],
+      )),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.handshake), label: 'bookings'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.settings), label: 'settings'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'profile'),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.indigo,
+        onTap: _ontapped,
+      ),
     );
   }
-  
 }
